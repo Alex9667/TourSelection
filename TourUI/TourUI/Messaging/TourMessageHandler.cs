1f864b89320270e22bc19190b62fb475ed9f9843
Alex9667 <blablabla2013@live.dk>
2024-09-18 21:31:03 +0200

    Added message model and messageHander

---
+++ TourUI/TourUI/Messaging/TourMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using TourUI.Model;
+
+namespace TourUI.Messaging
+{
+    public class TourMessageHandler
+    {
+        public void SendMessage(TourMessage tourMessage)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "tour_message", type: ExchangeType.Topic, durable: true);
+
+            var routingKey = tourMessage.Type;
+            var message = tourMessage.Name + " " + tourMessage.Email + " " + tourMessage.Location + " " + tourMessage.Type; 
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "tour_message",
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
+        }
+    }
+}
---
+++ TourUI/TourUI/Model/TourMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace TourUI.Model
+{
+    public class TourMessage
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Type { get; set; }
+
+        public TourMessage(string name, string email, string location, string type)
+        {
+            Name = name;
+            Email = email;
+            Location = location;
+            Type = type;
+        }
+    }
+}
